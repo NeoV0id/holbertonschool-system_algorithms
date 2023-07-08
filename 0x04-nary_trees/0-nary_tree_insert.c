f28216cfdea47330716ef8667ca1f4d6bbfba2b1
NeoV0id <jokerghoul.kpop@gmail.com>
2023-07-08 05:56:21 -0700

    [Try] trying with a tmp node

--- 0x04-nary_trees/0-nary_tree_insert.c
+++ 0x04-nary_trees/0-nary_tree_insert.c
@@ -3,6 +3,33 @@
 #include <stdio.h>
 #include <string.h>
 
+/**
+ * create_node - create a nary tree node
+ *
+ * @str: content of node
+ *
+ * Return: a new node
+ */
+nary_tree_t *create_node(char const *str)
+{
+	nary_tree_t *new_node;
+
+	new_node = malloc(sizeof(nary_tree_t));
+	if (new_node == NULL)
+		return (NULL);
+
+	if (str != NULL)
+		new_node->content = strdup(str);
+	else
+		new_node->content = NULL;
+
+	new_node->parent = NULL;
+	new_node->nb_children = 0;
+	new_node->children = NULL;
+	new_node->next = NULL;
+
+	return (new_node);
+}
 
 /**
  * nary_tree_insert - inserts node in a n-ary tree
@@ -15,41 +42,28 @@
 
 nary_tree_t *nary_tree_insert(nary_tree_t *parent, char const *str)
 {
-	char *dup;
 	nary_tree_t *tmp, *node;
 
-	dup = strdup(str);
-	tmp = malloc(sizeof(nary_tree_t));
-	if (tmp == NULL)
-		return (NULL);
-
-	node = malloc(sizeof(nary_tree_t));
-	if (node == NULL)
-		return (NULL);
-
-	node->content = dup;
+	tmp = create_node(NULL);
+	node = create_node(str);
 
 	if (parent == NULL)
-	{
-		node->parent = NULL;
-		node->children = NULL;
-		node->next = NULL;
 		return (node);
-	}
 
-	if (parent->children == NULL)
+	tmp = parent;
+	if (tmp->children == NULL)
 	{
-		parent->children = node;
+		tmp->children = node;
 		node->parent = parent;
-		parent->nb_children += 1;
+		tmp->nb_children += 1;
 		return (node);
 	}
-	if (parent->children != NULL)
+	if (tmp->children != NULL)
 	{
-		node->next = parent->children;
-		parent->children = node;
-		node->parent = parent;
-		parent->nb_children += 1;
+		node->next = tmp->children;
+		tmp->children = node;
+		node->parent = tmp;
+		tmp->nb_children += 1;
 		return (node);
 	}
 	return (NULL);
